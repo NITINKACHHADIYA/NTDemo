0fef715d7a3031d33a4e6c0516c40769da92055a
nitin kachhadiya <nitin.kachhadiyace@gmail.com>
2016-05-17 07:36:38 +0530

    Initial Commit

---
+++ GPSLocator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPSLocator
+//
+//  Created by nitin on 17/05/16.
+//  Copyright © 2016 organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPSLocator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GPSLocator
+//
+//  Created by nitin on 17/05/16.
+//  Copyright © 2016 organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
